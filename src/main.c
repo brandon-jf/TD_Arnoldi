8d2cf106ccb048b45da14d1ebac8b9b2efbd9c63
Brandon JEAN-FRANCOIS <brandon.jean-francois@ens.uvsq.fr>
2022-10-13 00:38:11 +0200

    Modification du main.c pour les mesures .

--- src/main.c
+++ src/main.c
@@ -4,24 +4,29 @@
 #include"arnoldi.h"
 #include<string.h>
 #include<time.h>
+#define BILLION 1E9
 
 
 int main(int argc, char** argv){
   int m,n,dim;
   double* matrice; // matrice a donné a l'algo
   char nom_matrice[50]; //nom du fichier qui contient la matrice
-  char first[30];
+  char first[50];
+  char name_file[50];
   double* v; // Vecteur initial
   double* Vm; //vecteur caculer par arnoldi
   int start, end;
-  unsigned long duree_ns;
+  unsigned long duree_ns[1000];
+  double duree_mean, variance;
   int duree_ipc;
   struct timespec t1,t2;
   FILE* fp;
   if(argc == 4){
    m = atoi(argv[1]);
     n = atoi(argv[2]);
-    memcpy(argv[3],nom_matrice,sizeof(double)*50);
+    memcpy(argv[3],nom_matrice,sizeof(char*)*50);
+    strncpy(name_file,argv[4],50);
+
     /*printf("Erreur : arnoldi [n] [m] [p] \n"
     "-dimention de la matrice [n]\n"
     "-dimension du sous-espace de Krylov [m]\n"
@@ -30,38 +35,51 @@ int main(int argc, char** argv){
   }
   n = atoi(argv[1]);
   m = atoi(argv[2]);
+  strncpy(name_file,argv[4],50);
   v = malloc(sizeof(double*)*m*m);
+  printf("Dim matrice %d | dim espace Krylov %d |Fichier mesure est : %s\n",n,m,argv[4]);
 
   init_vect(m,v);
   matrice = malloc(sizeof(double*)*n*n);
   printf("Mesure du temps d'éxecution de la reduction d'arnodli \n");
-  fp =fopen("./data/mesure.dat","a");
-  fscanf(fp,"%s\n",first);
-  if((strcmp(first,"# dimension\tdurée"))==0){
-    fprintf(fp,"# dimension\tduree\n");
-  }
-  start = clock_gettime(CLOCK_REALTIME, &t1);
-  for(int c = 0; c < 100; c ++){
+  fp =fopen(name_file,"a");
+  //fscanf(fp,"%s\n",first);
+//  if((strcmp(first,"# dimension\tdurée"))==0){
+//    fprintf(fp,"# dimension\tduree\n");
+//  }
+  duree_mean = 0;
+  variance = 0;
+  for(int c = 0; c < 1000; c ++){
+      start = clock_gettime(CLOCK_REALTIME, &t1);
       Vm = ArnoldiProjection ( n, m, v, matrice);
+      end = clock_gettime(CLOCK_REALTIME, &t2);
+      if ((t2.tv_nsec-t1.tv_nsec)<0)
+      {
+        duree_ns[c] = 1000000000+t2.tv_nsec-t1.tv_nsec;
+        duree_mean = duree_mean+duree_ns[c];
+      }
+      else
+      {
+        duree_ns[c] = t2.tv_nsec-t1.tv_nsec;
+        duree_mean = duree_mean+duree_ns[c];
+      }
+    //  variance = duree_ns[c]*duree_ns[c] + variance;
   }
-  end = clock_gettime(CLOCK_REALTIME, &t2);
-
-  if ((t2.tv_nsec-t1.tv_nsec)<0)
-  {
-    duree_ns = 1000000000+t2.tv_nsec-t1.tv_nsec;
-    duree_ns = duree_ns/100;
+  duree_mean = duree_mean/1000;
+  for(int c = 0; c < 1000; c ++){
+    variance = variance + (duree_ns[c]-duree_mean)*(duree_ns[c]-duree_mean);
+    printf("variance %ld\n",variance);
   }
-  else
-  {
-    duree_ns = t2.tv_nsec-t1.tv_nsec;
-    duree_ns = duree_ns/100;
-    }
+  variance = variance/1000;
+//  duree_ns = (double)(t2.tv_sec-t1.tv_sec)/100 + (t2.tv_nsec-t1.tv_nsec)/(double)BILLION;
 
-  fprintf(fp,"%d\t%ld\n",n,duree_ns);
+
+  fprintf(fp,"%d\t%ld\t%ld\n",n, duree_mean, variance);
   duree_ipc = (end - start)/100;
-  printf("Durée d'execution moyenne sur 1000 boucle : %ld ns \n", duree_ns);
+  printf("Durée d'execution moyenne sur 1000 boucle : %ld ns \n", duree_mean);
+  printf("variance de la Durée d'execution  sur 1000 boucle : %ld ns \n", variance);
 //  free(v);
-  //free(Vm);
+//  free(Vm);
 //  free(matrice);
   fclose(fp);
 
