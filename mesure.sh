6fc17920268939ccbe2e18b560ad7d942528e16d
Brandon JEAN-FRANCOIS <brandon.jean-francois@ens.uvsq.fr>
2022-10-13 00:36:53 +0200

    Modification du script du lancement de mesure : paramatrage corriger

--- mesure.sh
+++ mesure.sh
@@ -6,10 +6,10 @@
 #######################
 
 taille=20
-for i in {1..20};
+for i in {1..10};
 do
   dim=$(($i*100))&&
   m=10&&
   echo "Mesure du temps d'execution  n=" $dim " m=" $m&&
-  ./bin/arnoldi $dim $m;
+  ./bin/arnoldi $dim $m "matrice.mat" "./data/mesure3.dat"
 done
